cd867723fa2753c549819c83f547a6dc07180f26
Koji Igarashi <koji.igarashi.0719@gmail.com>
2020-04-20 19:44:26 +0900

    20200420

--- problems/ABC/163/d.cpp
+++ problems/ABC/163/d.cpp
@@ -4,44 +4,30 @@
 #include <functional>
 #include <math.h>
 
+typedef long long ll;
+const int MOD = (int)1e9 + 7;
+
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-int NCR(int n, int r) {
-  long long div = pow(10, 9) + 7;
+// ex1
+// int main() {
+//   ll n, k; cin >> n >> k;
+//   ll res = 0;
+//   for(int i=k; i<=n+1; ++i) {
+//     ll ans = i * n - pow(i, 2) + i + 1;
+//     res =  (res + ans) % MOD;
+//   }
+//   cout << res << endl;
+//   return 0;
+// }
 
-  if (r == 0) return 1;
-  if (r > n / 2) return NCR(n, n - r);
-
-  long res = 1;
-  for (int k = 1; k <= r; ++k) {
-      res *= n - k + 1;
-      res /= k;
-      res = res % div;
-  }
-  return res;
-}
-
-
+// ex2
 int main() {
-  int n, k; cin >> n >> k;
-
-  long long p = pow(10, 100);
-
-  if (n == (k - 1)) {
-    cout << 1 << endl;
-    return 0;
-  }
-
-  long long res = 0;
-  rep(i, n-k+1) {
-    res += NCR(n+1, k+i);
-  }
-
-  // res += NCR(n+1, k);
-
-
-  cout << res << endl;
+  ll n, k; cin >> n >> k;
+  ll M = n+1, L = k-1;
+  ll res = M*(M*M+5)/6 + L*(L*L*2+L*3-5-M*3*(L+1))/6;
+  cout << res % MOD << endl;
   return 0;
 }
