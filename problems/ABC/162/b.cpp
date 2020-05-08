2cf948d372cc2a347435b188075bbb5711d7142a
Koji Igarashi <koji.igarashi.0719@gmail.com>
2020-05-08 18:49:47 +0900

    20200505

---
+++ problems/ABC/162/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define REP(i,n) for(ll i=0;i<(ll)(n);i++)
+#define REPD(i,n) for(ll i=(ll)(n)-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define ALL(x) (x).begin(),(x).end()
+#define SIZE(x) ((ll)(x).size())
+#define MAX(x) *max_element(ALL(x))
+#define SUM(x) accumulate(ALL(x), 0)
+#define INF 1000000000000
+#define MOD 10000007
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define MAXR 100000
+
+int main()
+{
+  ll N; cin >> N;
+  vector<ll> dp(1000010, 0);
+  dp.at(0) = 0;
+  FOR(i, 1, N) {
+    if (i % 3 == 0 || i % 5 == 0) dp.at(i) = dp.at(i - 1);
+    else dp.at(i) = dp.at(i - 1) + i;
+  }
+  cout << dp.at(N) << endl;
+  return 0;
+}
