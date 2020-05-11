4fbfccba9cabee81ddc1258be14e08115f7c3d6b
Koji Igarashi <koji.igarashi.0719@gmail.com>
2020-05-11 20:21:00 +0900

    add

--- template.cpp
+++ template.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
 
 #define REP(i,n) for(ll i=0;i<(ll)(n);i++)
 #define REPD(i,n) for(ll i=(ll)(n)-1;i>=0;i--)
@@ -11,26 +13,19 @@ typedef long long ll;
 #define SIZE(x) ((ll)(x).size())
 #define MAX(x) *max_element(ALL(x))
 #define SUM(x) accumulate(ALL(x), 0)
-#define INF 1000000000000
-#define MOD 10000007
+#define print(x) cout << x;
+#define println(x) cout << x << endl;
+#define printd(x, f) cout << fixed << setprecision(f) << x << endl;
+
 #define PB push_back
 #define MP make_pair
 #define F first
 #define S second
+
+#define INF 1000000000000
+#define MOD 10000007
 #define MAXR 100000
 #define PI acos(-1.0)
-
-template <typename T>
-void printVector(const std::vector<T> &data)
-{
-    stringstream ss;
-    ostream_iterator<T> out_it(ss, ", ");
-    ss << "[";
-    copy(ALL(data) - 1, out_it);
-    ss << data.back() << "]";
-    cout << ss.str() << endl;
-    return;
-}
 
 int main()
 {
