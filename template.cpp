e8b04e4ebe8760c206c9d7fbd5f0fed1870fe474
Koji Igarashi <koji.igarashi.0719@gmail.com>
2020-05-20 23:44:58 +0900

    fix emplate

--- template.cpp
+++ template.cpp
@@ -9,13 +9,14 @@ typedef vector<ll> vll;
 #define REPD(i,n) for(ll i=(ll)(n)-1;i>=0;i--)
 #define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
 #define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define FORMAP(x, mp) for(auto x=mp.begin();x!=mp.end();x++)
 #define ALL(x) (x).begin(),(x).end()
 #define SIZE(x) ((ll)(x).size())
 #define MAX(x) *max_element(ALL(x))
 #define SUM(x) accumulate(ALL(x), 0)
-#define print(x) cout << x;
-#define println(x) cout << x << endl;
-#define printd(x, f) cout << fixed << setprecision(f) << x << endl;
+#define print(x) cout << x
+#define println(x) cout << x << endl
+#define printd(x, f) cout << fixed << setprecision(f) << x << endl
 
 #define PB push_back
 #define MP make_pair
